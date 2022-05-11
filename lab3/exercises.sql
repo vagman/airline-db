d31c20c6d0458ac959a8a9bd67bd2a6338cf58c0
Vaggelis Manousakis <bmanousakis@gmail.com>
2022-05-11 16:34:52 +0300

    finished lab3

--- lab3/exercises.sql
+++ lab3/exercises.sql
@@ -75,8 +75,69 @@ from teaches
 where semester = 'Spring' and year = 2010;
 
 
+-- Βρείτε το Μ.Ο. των μισθών κάθε τμήματος.
+select dept_name, round(avg(salary)) as average_salary
+from instructor
+group by dept_name;
 
+-- Βρείτε τον αριθμό των καθηγητών σε κάθε τμήμα που δίδαξαν ένα μάθημα την Άνοιξη του 2010.
+select dept_name, count(I.id) as sum_of_spring2010_instructors
+from instructor as I natural join teaches as T
+where T.semester = 'Spring' and T.year = '2010'
+group by dept_name;
 
+-- Βρείτε τα τμήματα όπου ο Μ.Ο. των μισθών των καθηγητών να είναι μεγαλύτερος από 42000.
+select dept_name, round(avg(salary)) as average_salary_above_42k
+from instructor
+group by dept_name
+having  avg(salary) >= 42000;
 
+-- Βρείτε το άθροισμα του πλυθισμού ανά περιφέρεια (district). Να γίνει φθίνουσα αναπαράσταση 
+-- του αποτελέσματος των 15 πρότων 
+select district, sum(population) as district_population
+from city
+group by district
+order by district_population desc limit 15;
 
+-- Βρείτε τα μαθήματα που διδάχθηκαν το Φθινόπωρο του 2009 & την Άνοιξη του 2010.
+(select course_id as fall_2009_courses
+from section
+where semester = 'Fall' and year = '2009' 
+group by course_id)
+intersect
+(select course_id
+from section
+where semester = 'Spring' and year = '2010'
+group by course_id
+);
+
+-- Another way with keyword "IN"...
+select course_id as fall_2009_courses
+from section
+where semester = 'Spring' and year = 2010 and course_id in
+    (select course_id
+    from section
+    where semester = 'Fall' and year = 2009);
+
+-- Βρείτε τα ονόματα των καθηγητών των οποίων ο μισθός είναι μεγαλύτερος από
+-- τουλάχιστον έναν καθηγητή του τμήματος Βιολογίας.
+select I1.name, I1.salary
+from instructor as I1, instructor as I2	
+where I1.salary > I2.salary and I2.dept_name = 'Biology';
+
+-- Another way
+select name, salary
+from instructor
+where salary > some
+	(select salary
+	from instructor
+	where dept_name = 'Biology');
+
+-- Βρείτε τις πόλεις που ανεξαρτητιποιήθηκαν μετά το 1920 και βρίσκονται στην Ασιατική ύπειρο
+select name
+from city
+where countryCode in 
+	(select code
+	from country
+	where indepYear > 1920 and continent = 'Asia');
 
