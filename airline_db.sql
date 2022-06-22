55fe826f6c0f6caea82a768b61f8e057909742be
Vaggelis Manousakis <bmanousakis@gmail.com>
2022-06-22 20:59:04 +0300

    Small changes to the tables - FKs needs to be fixed

--- airline_db.sql
+++ airline_db.sql
@@ -3,10 +3,10 @@ CREATE TABLE IF NOT EXISTS booking
 	book_ref VARCHAR(6),
 	book_date TIMESTAMPTZ NOT NULL,
 	total_cost DECIMAL(6,2) NOT NULL,
-	ticket_no VARCHAR(13) NOT NULL UNIQUE,
 
 	CHECK (book_ref ~* '([a-z]|[A-Z]|\d){6}'), -- A combination of 6 digits or numbers
 	CHECK (total_cost > 0),
+
 	PRIMARY KEY (book_ref)
 );
 
@@ -15,9 +15,7 @@ CREATE TABLE IF NOT EXISTS boarding_pass
 	flight_id SERIAL,
 	seat_no VARCHAR(3),
 	boarding_no SERIAL NOT NULL,
-	passenger_name VARCHAR(100) NOT NULL,
 	ticket_no VARCHAR(13) NOT NULL UNIQUE,
-	aircraft_code VARCHAR(3) NOT NULL UNIQUE,
 
 	PRIMARY KEY (flight_id, seat_no)
 );
@@ -25,13 +23,11 @@ CREATE TABLE IF NOT EXISTS boarding_pass
 CREATE TABLE IF NOT EXISTS ticket
 (
 	ticket_no VARCHAR(13),
-	passenger_id VARCHAR(10) NOT NULL UNIQUE,
-	passenger_name VARCHAR(100) NOT NULL,
-	contact_data VARCHAR(100) NOT NULL,
 	amount DECIMAL(6,2) NOT NULL,
 	fare VARCHAR(11) NOT NULL,
 	book_ref VARCHAR(6) NOT NULL UNIQUE,
 	flight_id INT NOT NULL UNIQUE,
+	passenger_id VARCHAR(10) NOT NULL UNIQUE,
 
 	CHECK (ticket_no ~* '^\d{13}$'), -- It has to be a 13-digit number
 	CHECK (fare IN ('Economy', 'Business', 'First class')),
@@ -39,8 +35,18 @@ CREATE TABLE IF NOT EXISTS ticket
 	CHECK (amount > 0),
 
 	FOREIGN KEY (ticket_no) REFERENCES boarding_pass(ticket_no) ON DELETE CASCADE,
-	FOREIGN KEY (ticket_no) REFERENCES booking(ticket_no) ON DELETE CASCADE,
+	FOREIGN KEY (book_ref) REFERENCES booking(book_ref) ON DELETE CASCADE,
 	PRIMARY KEY (ticket_no)
+);
+
+CREATE TABLE IF NOT EXISTS passenger
+(
+	passenger_id VARCHAR(10),
+	passenger_name VARCHAR(100),
+	contact_data VARCHAR(100),
+
+	PRIMARY KEY (passenger_id),
+	FOREIGN KEY (passenger_id) REFERENCES ticket(passenger_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS flight
@@ -53,18 +59,19 @@ CREATE TABLE IF NOT EXISTS flight
 	distance NUMERIC(4,0) NOT NULL,
 	scheduled_departure_time TIMESTAMPTZ NOT NULL,
 	scheduled_arrival_time TIMESTAMPTZ NOT NULL,
-	scheduled_duration numeric(4,2) NOT NULL generated always as ( extract(epoch from (scheduled_arrival_time - scheduled_departure_time))/3600 ) stored,
+	scheduled_duration numeric(4,2), -- NOT NULL generated always as ( extract(epoch from (scheduled_arrival_time - scheduled_departure_time))/3600 ) stored,
 	actual_departure_time TIMESTAMPTZ,
 	actual_arrival_time TIMESTAMPTZ,
 	flight_status VARCHAR(9) NOT NULL,
 	aircraft_code VARCHAR(3) NOT NULL UNIQUE,
-	airport_code VARCHAR(3) NOT NULL UNIQUE,
 
 	CHECK (arrival_airport != departure_airport), -- May need a trigger
 	CHECK ((actual_arrival_time IS NULL) OR ((actual_departure_time IS NOT NULL AND  actual_arrival_time IS NOT NULL) AND (scheduled_arrival_time > scheduled_departure_time))),
 	CHECK (flight_status IN ('Scheduled', 'OnTime', 'Delayed', 'Departed', 'Arrived', 'Cancelled')),
 
 	FOREIGN KEY (flight_id) REFERENCES ticket(flight_id) ON DELETE CASCADE,
+	FOREIGN KEY(arrival_airport), 
+	FOREIGN KEY(departure_airport) REFERENCES airport(airport_code) ON DELETE CASCADE,
 	PRIMARY KEY (flight_id)
 );
 
@@ -96,3 +103,22 @@ CREATE TABLE IF NOT EXISTS airport
 	FOREIGN KEY (airport_code) REFERENCES flight(airport_code) ON DELETE CASCADE,
 	PRIMARY KEY (airport_code)
 );
+
+-- Sample data
+COPY booking FROM '/booking.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM booking;
+
+COPY flight FROM '/flight.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM flight;
+
+COPY airport FROM '/airport.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM airport;
+
+COPY aircraft FROM '/aircraft.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM aircraft;
+
+COPY ticket FROM '/ticket.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM ticket;
+
+COPY boarding_pass FROM '/boarding_pass.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM boarding_pass;
--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,13 @@
 -- Here we will implement Ερώτημα 3 (20%) - Triggers
 
--- Trigger for aircraft_range > flight.distance:
+-- Check that the aircraft_distance is greater than flight.distance
+CREATE FUNCTION flight_distance() RETURNS trigger AS $flight_distance$
+    BEGIN
+        IF NEW.aircraft.aircraft_distance < NEW.flight.distance THEN
+            RAISE EXCEPTION '% cannot have a flight distance with less that the aircrafts distance', NEW.;
+        END IF;
+    END;
+$flight_distance$ LANGUAGE plpgsql;
+
+CREATE TRIGGER flight_distance BEFORE INSERT OR UPDATE 
+ON flight FOR EACH ROW EXECUTE PROCEDURE flight_distance();
