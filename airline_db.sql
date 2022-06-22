49b758f40ed6d2222f433194ac4f8c671f996dc1
Kostas Gialantzis <kgialant@gmail.com>
2022-06-22 21:44:29 +0300

    syntax changes

--- airline_db.sql
+++ airline_db.sql
@@ -10,14 +10,57 @@ CREATE TABLE IF NOT EXISTS booking
 	PRIMARY KEY (book_ref)
 );
 
-CREATE TABLE IF NOT EXISTS boarding_pass
+CREATE TABLE IF NOT EXISTS aircraft
+(
+	aircraft_code VARCHAR(3),
+	aircraft_model VARCHAR(40) NOT NULL,
+	capacity INT NOT NULL,
+	aircraft_range NUMERIC(4,0) NOT NULL, 
+
+	CHECK (aircraft_code ~* '^\d{3}$'), -- It has to be a 3-digit number
+	CHECK (capacity > 0),
+	CHECK (aircraft_range > 0),
+
+	PRIMARY KEY (aircraft_code)
+);
+
+CREATE TABLE IF NOT EXISTS airport
+(
+	airport_code VARCHAR(3),
+	airport_name VARCHAR(100) NOT NULL,
+	city VARCHAR(100) NOT NULL,
+	timezone TIMESTAMPTZ NOT NULL,
+
+	CHECK (airport_code ~* '^\w{3}$'), -- It has to be a 3 letters
+
+	PRIMARY KEY (airport_code)
+);
+
+
+CREATE TABLE IF NOT EXISTS flight
 (
 	flight_id SERIAL,
-	seat_no VARCHAR(3),
-	boarding_no SERIAL NOT NULL,
-	ticket_no VARCHAR(13) NOT NULL UNIQUE,
+	arrival_airport VARCHAR(100), -- May need a trigger
+	departure_airport VARCHAR(100), -- May need a trigger
+	departure_date DATE, -- May need a trigger
+	distance NUMERIC(4,0) NOT NULL,
+	scheduled_departure_time TIMESTAMPTZ NOT NULL,
+	scheduled_arrival_time TIMESTAMPTZ NOT NULL,
+	scheduled_duration numeric(4,2), -- NOT NULL generated always as ( extract(epoch from (scheduled_arrival_time - scheduled_departure_time))/3600 ) stored,
+	actual_departure_time TIMESTAMPTZ,
+	actual_arrival_time TIMESTAMPTZ,
+	flight_status VARCHAR(9) NOT NULL,
+	aircraft_code VARCHAR(3) NOT NULL UNIQUE,
 
-	PRIMARY KEY (flight_id, seat_no)
+	CHECK (arrival_airport != departure_airport), -- May need a trigger
+	CHECK ((actual_arrival_time IS NULL) OR ((actual_departure_time IS NOT NULL AND  actual_arrival_time IS NOT NULL) AND (scheduled_arrival_time > scheduled_departure_time))),
+	CHECK (flight_status IN ('Scheduled', 'OnTime', 'Delayed', 'Departed', 'Arrived', 'Cancelled')),
+
+	
+	FOREIGN KEY(aircraft_code) REFERENCES aircraft(aircraft_code) ON DELETE CASCADE,
+	FOREIGN KEY(arrival_airport) REFERENCES airport(airport_code) ON DELETE CASCADE, -- changes for discussion
+	FOREIGN KEY(departure_airport) REFERENCES airport(airport_code) ON DELETE CASCADE,
+	PRIMARY KEY (flight_id)
 );
 
 CREATE TABLE IF NOT EXISTS ticket
@@ -34,75 +77,38 @@ CREATE TABLE IF NOT EXISTS ticket
 	CHECK (book_ref ~* '([a-z]|[A-Z]|\d){6}'),
 	CHECK (amount > 0),
 
-	FOREIGN KEY (ticket_no) REFERENCES boarding_pass(ticket_no) ON DELETE CASCADE,
+	FOREIGN KEY (flight_id) REFERENCES flight(flight_id) ON DELETE CASCADE,
 	FOREIGN KEY (book_ref) REFERENCES booking(book_ref) ON DELETE CASCADE,
 	PRIMARY KEY (ticket_no)
 );
 
-CREATE TABLE IF NOT EXISTS passenger
+CREATE TABLE IF NOT EXISTS boarding_pass
+(
+	flight_id SERIAL,
+	seat_no VARCHAR(3),
+	boarding_no SERIAL NOT NULL,
+	ticket_no VARCHAR(13) NOT NULL UNIQUE,
+
+	FOREIGN KEY (ticket_no) REFERENCES ticket(ticket_no) ON DELETE CASCADE,
+	FOREIGN KEY (flight_id) REFERENCES flight(flight_id) ON DELETE CASCADE,
+	PRIMARY KEY (flight_id, seat_no)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS passenger -- Look the description about ticket(passenger_id) table at POSTGRESPRO DOCS
 (
 	passenger_id VARCHAR(10),
 	passenger_name VARCHAR(100),
 	contact_data VARCHAR(100),
 
-	PRIMARY KEY (passenger_id),
-	FOREIGN KEY (passenger_id) REFERENCES ticket(passenger_id) ON DELETE CASCADE
+	
+	FOREIGN KEY (passenger_id) REFERENCES ticket(passenger_id) ON DELETE CASCADE,
+	PRIMARY KEY (passenger_id) --check if primary key can be {passenger_id, passenger_name, contact_data}
 );
 
-CREATE TABLE IF NOT EXISTS flight
-(
-	flight_id SERIAL,
-	arrival_airport VARCHAR(100), -- May need a trigger
-	departure_airport VARCHAR(100), -- May need a trigger
-	departure_date DATE, -- May need a trigger
-	airplane_model VARCHAR(40) NOT NULL,
-	distance NUMERIC(4,0) NOT NULL,
-	scheduled_departure_time TIMESTAMPTZ NOT NULL,
-	scheduled_arrival_time TIMESTAMPTZ NOT NULL,
-	scheduled_duration numeric(4,2), -- NOT NULL generated always as ( extract(epoch from (scheduled_arrival_time - scheduled_departure_time))/3600 ) stored,
-	actual_departure_time TIMESTAMPTZ,
-	actual_arrival_time TIMESTAMPTZ,
-	flight_status VARCHAR(9) NOT NULL,
-	aircraft_code VARCHAR(3) NOT NULL UNIQUE,
 
-	CHECK (arrival_airport != departure_airport), -- May need a trigger
-	CHECK ((actual_arrival_time IS NULL) OR ((actual_departure_time IS NOT NULL AND  actual_arrival_time IS NOT NULL) AND (scheduled_arrival_time > scheduled_departure_time))),
-	CHECK (flight_status IN ('Scheduled', 'OnTime', 'Delayed', 'Departed', 'Arrived', 'Cancelled')),
 
-	FOREIGN KEY (flight_id) REFERENCES ticket(flight_id) ON DELETE CASCADE,
-	FOREIGN KEY(arrival_airport), 
-	FOREIGN KEY(departure_airport) REFERENCES airport(airport_code) ON DELETE CASCADE,
-	PRIMARY KEY (flight_id)
-);
-
-CREATE TABLE IF NOT EXISTS aircraft
-(
-	aircraft_code VARCHAR(3),
-	model VARCHAR(40) NOT NULL,
-	capacity INT NOT NULL,
-	aircraft_range NUMERIC(4,0) NOT NULL, 
-
-	CHECK (aircraft_code ~* '^\d{3}$'), -- It has to be a 3-digit number
-	CHECK (capacity > 0),
-	CHECK (aircraft_range > 0),
-
-	FOREIGN KEY (aircraft_code) REFERENCES flight(aircraft_code) ON DELETE CASCADE,
-	FOREIGN KEY (aircraft_code) REFERENCES boarding_pass(aircraft_code) ON DELETE CASCADE,
-	PRIMARY KEY (aircraft_code)
-);
-
-CREATE TABLE IF NOT EXISTS airport
-(
-	airport_code VARCHAR(3),
-	airport_name VARCHAR(100) NOT NULL,
-	city VARCHAR(100) NOT NULL,
-	timezone TIMESTAMPTZ NOT NULL,
-
-	CHECK (airport_code ~* '^\w{3}$'), -- It has to be a 3 letters
-
-	FOREIGN KEY (airport_code) REFERENCES flight(airport_code) ON DELETE CASCADE,
-	PRIMARY KEY (airport_code)
-);
 
 -- Sample data
 COPY booking FROM '/booking.csv' DELIMITER ',' CSV HEADER;
@@ -119,6 +125,9 @@ SELECT * FROM aircraft;
 
 COPY ticket FROM '/ticket.csv' DELIMITER ',' CSV HEADER;
 SELECT * FROM ticket;
+
+COPY ticket FROM '/passenger.csv' DELIMITER ',' CSV HEADER;
+SELECT * FROM passenger;
 
 COPY boarding_pass FROM '/boarding_pass.csv' DELIMITER ',' CSV HEADER;
 SELECT * FROM boarding_pass;
